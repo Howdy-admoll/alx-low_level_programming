a0897c896587ce1993749766e711015c323976a6
Howdy-admoll <admoll.adefemi@gmail.com>
2022-04-12 23:44:15 +0100

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,8 +7,16 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
