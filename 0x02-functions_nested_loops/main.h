1df1268fdbbf80d999f47a57bbc9430359344e26
Howdy-admoll <admoll.adefemi@gmail.com>
2022-04-12 21:51:45 +0100

    updated

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_INCLUDED 
+#define MAIN_H_INCLUDED
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isaplha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_tables(int n);
+#endif
