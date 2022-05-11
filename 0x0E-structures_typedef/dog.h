ff247658731cf84608cb2dee273a0e3bf664a951
Howdy-admoll <admoll.adefemi@gmail.com>
2022-05-11 22:49:56 +0100

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+* typedef dog_t - defines a new name for type struct dog
+*/
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
